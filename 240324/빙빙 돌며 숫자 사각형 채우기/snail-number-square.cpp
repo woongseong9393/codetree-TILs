aeb8245c773df8310e37bbef47e34c8fad79d65b
woongseong9393 <130616834+woongseong9393@users.noreply.github.com>
2024-03-24 17:55:17 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240324/빙빙 돌며 숫자 사각형 채우기/snail-number-square.cpp
+++ 240324/빙빙 돌며 숫자 사각형 채우기/snail-number-square.cpp
@@ -16,19 +16,17 @@ int main() {
     // 여기에 코드를 작성해주세요.
     cin >> n >> m;
     int dir = 1;
-    int cnt = 1;
     int x = 0, y = 0;
 
-    while(map[y][x] == 0){
-        map[y][x] = cnt++;
-        if(!(inRange(x + dx[dir], y + dy[dir])) || map[y + dy[dir]][x + dx[dir]] != 0){
+    for(int idx = 1; idx <= n * m; idx++){
+        map[y][x] = idx;
+        int nextX = x + dx[dir];
+        int nextY = y + dy[dir];
+        if(!inRange(nextX, nextY) || map[nextY][nextX] != 0){
             dir = (dir + 1) % 4;
-            x += dx[dir];
-            y += dy[dir];
-        } else{
-            x += dx[dir];
-            y += dy[dir];
         }
+        x = x + dx[dir];
+        y = y + dy[dir];
     }
 
     for(int i = 0; i < n; i++){
