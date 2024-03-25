44951111d39c6a39cef244874e8c6b37b165c2f0
woongseong9393 <130616834+woongseong9393@users.noreply.github.com>
2024-03-25 13:25:59 +0900

    Title: 격자 위의 편안한 상태, Time: 1087ms, Memory: 18MB, Status: Time Limit Exceed - CodeTree

---
+++ 240325/격자 위의 편안한 상태/comfortable-state-on-the-grid.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#define MAX_NUM 100
+
+using namespace std;
+
+int N, M;
+int r, c;
+
+/*N E S W*/
+int dx[4] = {0, 1, 0, -1};
+int dy[4] = {1, 0, -1, 0};
+
+bool mp[MAX_NUM][MAX_NUM] = {0,};
+
+bool inRange(int r, int c){
+    return 0 <= r && r < N && 0 <= c && c <N;
+}
+
+bool chk_state(int r, int c){
+    int nextR, nextC;
+    int cnt = 0;
+
+    for(int i = 0; i < 4; i++){
+        nextR = r + dy[i];
+        nextC = c + dx[i];
+        if(mp[nextR][nextC]) cnt++;
+    }
+
+    if(cnt == 3){
+        return 1;
+    }
+
+    return 0;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    // 여기에 코드를 작성해주세요.
+    cin >> N >> M;
+    for(int i = 0; i < M; i++){
+        cin >> r >> c;
+        mp[r][c] = 1;
+
+        cout << chk_state(r, c) << '\n';
+    }
+
+    return 0;
+}
