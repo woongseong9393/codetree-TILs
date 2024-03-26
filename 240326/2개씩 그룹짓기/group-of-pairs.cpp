b3ec8851fdd98dcb6966f8bf687fe9575ddc0c10
woongseong9393 <130616834+woongseong9393@users.noreply.github.com>
2024-03-26 23:46:33 +0900

    Title: 2개씩 그룹짓기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240326/2개씩 그룹짓기/group-of-pairs.cpp
+++ 240326/2개씩 그룹짓기/group-of-pairs.cpp
@@ -11,14 +11,15 @@ vector<int> v;
 int main() {
     // 여기에 코드를 작성해주세요.
     cin >> n;
-    for(int i = 0; i < n; i++){
+    for(int i = 0; i < n * 2; i++){
         cin >> arr[i];
     }
 
-    sort(arr, arr + n);
+    sort(arr, arr + n * 2);
 
-    for(int i = 0; i < n / 2; i++){
-        v.push_back(arr[i] + arr[n-1-i]);
+    for(int i = 0; i < n ; i++){
+        v.push_back(arr[i] + arr[2 * n -1 -i]);
+        //cout << v[i] << endl;
     }
 
     int max = *max_element(v.begin(), v.end());
