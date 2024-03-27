3fece89a7f4891d03aaaf015f450d14d0dfb4f68
woongseong9393 <130616834+woongseong9393@users.noreply.github.com>
2024-03-27 14:39:30 +0900

    Title: 겹치지 않게 선분 고르기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240327/겹치지 않게 선분 고르기/select-segments-without-overlap.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n;
+int max_lines = 0;
+
+vector<pair<int, int>> lines;
+vector<int> choosed;
+
+bool chk_avail(int line_idx){
+    if(choosed.size()==0){
+        return true;
+    }
+    for(const auto& element : choosed){
+        if(lines[element].first <= lines[line_idx].first && lines[element].second >= lines[line_idx].second){
+            return false;
+        }
+    }
+    return true;
+}
+
+void Choose(int line_idx){
+    if(line_idx == n){
+        if(max_lines < choosed.size()) max_lines = choosed.size();
+        return;
+    }
+
+    if(chk_avail(line_idx)){
+        choosed.push_back(line_idx);
+        Choose(line_idx + 1);
+        choosed.pop_back();
+    } else{
+        Choose(line_idx + 1);
+    }
+
+    return;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n;
+    int x1, x2;
+    for(int i = 0; i < n; i++){
+        cin >> x1 >> x2;
+        lines.push_back(make_pair(x1, x2));
+    }
+
+    Choose(0);
+    cout << max_lines;
+
+    return 0;
+}
