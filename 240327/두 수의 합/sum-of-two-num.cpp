71f57812ce1928e3b80a00e647bffc9ec1978767
woongseong9393 <130616834+woongseong9393@users.noreply.github.com>
2024-03-27 21:24:26 +0900

    Title: 두 수의 합, Time: 71ms, Memory: 3MB, Status: Passed - CodeTree

--- 240327/두 수의 합/sum-of-two-num.cpp
+++ 240327/두 수의 합/sum-of-two-num.cpp
@@ -15,9 +15,12 @@ int main() {
     // 여기에 코드를 작성해주세요.
     cin >> n >> k;
 
+    //n = 5;
+    //k = 6;
     for(int i = 0 ; i < n; i++){
         cin >> arr[i];
-        if(um.find(arr[i])!= um.end()){
+        //arr[i] = 3;
+        if(um.find(arr[i])== um.end()){
             um[arr[i]] = 1;
         } else{
             um[arr[i]]++;
@@ -26,10 +29,12 @@ int main() {
 
     for(int i = 0; i < n; i++){
         int look = k - arr[i];
+        //cout << um[arr[i]] << endl;
         if(um[arr[i]] > 0){
             um[arr[i]]--;
             if(um.find(look) != um.end()){
                 cnt += um[look];
+                //cout << um[look] << endl;
             }
         }
     }
