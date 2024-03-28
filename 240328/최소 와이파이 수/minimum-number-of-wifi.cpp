db57f730cfddd191169bc39c1930633ccb029fa6
woongseong9393 <130616834+woongseong9393@users.noreply.github.com>
2024-03-28 17:26:52 +0900

    Title: 최소 와이파이 수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240328/최소 와이파이 수/minimum-number-of-wifi.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n, m;
+
+int ppl[100] = {0,};
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n >> m;
+    for(int i = 0; i < n; i++){
+        cin >> ppl[i];
+    }
+
+    int ans = 0;
+    int wifi_s = 0, wifi_f = 0;
+    for(int i = 0; i < n; i++){
+        if(ppl[i] == 1 && wifi_f < i){
+            wifi_s = i + m;
+            wifi_f = wifi_s + m;
+            ans++;
+        }
+    }
+
+    cout << ans;
+
+    return 0;
+}
