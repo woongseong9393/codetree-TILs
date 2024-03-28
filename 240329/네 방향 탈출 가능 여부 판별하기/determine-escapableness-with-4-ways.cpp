67876c105b152e4ad9854e23079ee59d6db880eb
woongseong9393 <130616834+woongseong9393@users.noreply.github.com>
2024-03-29 01:56:26 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

--- 240329/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.cpp
+++ 240329/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.cpp
@@ -42,7 +42,7 @@ int main() {
     // 여기에 코드를 작성해주세요.
     cin >> n >> m;
     for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
+        for(int j = 0; j < m; j++){
             cin >> map[i][j];
         }
     }
