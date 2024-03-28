47b502eab0998062e82316bdbbcc677011cdc05f
woongseong9393 <130616834+woongseong9393@users.noreply.github.com>
2024-03-28 12:49:51 +0900

    Title: 만나는 그 순간, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240328/만나는 그 순간/the-moment-we-meet.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+int N, M, dist;
+char dir;
+
+int A[1001] = {0,};
+int B[1001] = {0,};
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> N >> M;
+    int idx_A = 1;
+    for(int i = 0; i < N; i++){
+        cin >> dir >> dist;
+        for(int k = 0; k < dist; k++){
+            if(dir == 'L'){
+                A[idx_A] = A[idx_A - 1] - 1;
+            } else{
+                A[idx_A] = A[idx_A - 1] + 1;
+            }
+            idx_A++;
+        }
+    }
+
+    int idx_B = 1;
+    for(int i = 0; i < M; i++){
+        cin >> dir >> dist;
+        for(int k = 0; k < dist; k++){
+            if(dir == 'L'){
+                B[idx_B] = B[idx_B - 1] - 1;
+            } else{
+                B[idx_B] = B[idx_B - 1] + 1;
+            }
+            idx_B++;
+        }
+    }
+
+    int t = idx_A < idx_B ? idx_A : idx_B;
+
+    bool chk = 0;
+    for(int i = 1; i <= t; i++){
+        if(A[i] == B[i]){
+            cout << i;
+            chk = 1;
+            break;
+        }
+    }
+
+    if(!chk){
+        cout << -1;
+    }
+
+    return 0;
+}
