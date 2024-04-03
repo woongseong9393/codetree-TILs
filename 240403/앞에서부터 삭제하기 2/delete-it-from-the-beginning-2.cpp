6e7bc8b5544bd3ea994c78e522f1c5d45da2cc30
woongseong9393 <130616834+woongseong9393@users.noreply.github.com>
2024-04-03 14:38:22 +0900

    Title: 앞에서부터 삭제하기 2, Time: 35ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 240403/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstdio>
+#include <queue>
+#include <climits>
+
+using namespace std;
+
+int N;
+long long sum = 0;
+double avg = INT_MIN;
+
+priority_queue<int> pq;
+int arr[100000];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> N;
+    for(int i = 0; i < N; i++){
+        cin >> arr[i];
+        pq.push(-arr[i]);
+        sum += arr[i];
+    }
+
+    for(int i = 0; i < N - 2; i++){
+        int temp_sum;
+        sum -= arr[i];
+        if(-arr[i] == pq.top()){
+            pq.pop();
+        }
+        temp_sum = sum + pq.top();
+        avg = avg > temp_sum / (N - i - 2) ? avg : temp_sum / (N - i - 2);
+    }
+
+    printf("%.2lf", avg);
+
+    return 0;
+}
