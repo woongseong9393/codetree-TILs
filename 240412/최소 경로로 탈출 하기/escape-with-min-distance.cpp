72fec96b88d3e1f640ce68254a322711d6e6f25d
woongseong9393 <130616834+woongseong9393@users.noreply.github.com>
2024-04-12 19:58:49 +0900

    Title: 최소 경로로 탈출 하기, Time: 11ms, Memory: 0MB, Status: Passed - CodeTree

--- 240412/최소 경로로 탈출 하기/escape-with-min-distance.cpp
+++ 240412/최소 경로로 탈출 하기/escape-with-min-distance.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <algorithm>
 
 using namespace std;
 
 int N, M;
 int mp[100][100] = {0,};
-int visited[100][100] = {0,};
+int step[100][100] = {0,};
+bool visited[100][100] = {0,};
 
 int dx[4] = {0, 1, 0, -1};
 int dy[4] = {1, 0, -1, 0};
@@ -17,19 +19,27 @@ bool inRange(int x, int y){
     return 0 <= x && x < M && 0 <= y && y < N;
 }
 
-void BFS(int x, int y, int step){
-    if(x == M-1 && y == N-1){
-        return;
-    }
-    for(int i = 0; i < 4; i++){
-        int nx = x + dx[i];
-        int ny = y + dy[i];
 
-        if(inRange(nx, ny) && mp[ny][nx] == 1 && visited[ny][nx] <= 0){
-            visited[ny][nx] = step + 1;
-            BFS(nx, ny, step + 1);
+
+void BFS(){
+    while(!q.empty()){
+        int curr_x = q.front().first;
+        int curr_y = q.front().second;
+        q.pop();
+
+        for(int i = 0; i < 4; i++){
+            int nx = curr_x + dx[i];
+            int ny = curr_y + dy[i];
+
+            if(inRange(nx, ny) && visited[ny][nx] == 0 && mp[ny][nx] == 1){
+                visited[ny][nx] = true;
+                step[ny][nx] = step[curr_y][curr_x] + 1;
+                q.push(make_pair(nx, ny));
+            }
         }
     }
+
+    return;
 }
 
 int main() {
@@ -41,11 +51,21 @@ int main() {
         }
     }
 
-    visited[0][0] = 0;
-    visited[N-1][M-1] = -1;
-    BFS(0, 0, 0);
+    visited[0][0] = 1;
+    step[N-1][M-1] = -1;
+    q.push({0, 0});
+    BFS();
 
-    cout << visited[N-1][M-1]; 
+    /*
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < M; j++){
+            cout << visited[i][j] << ' ';
+        }
+        cout << endl;
+    }
+    */
+    
+    cout << step[N-1][M-1]; 
 
     return 0;
 }
