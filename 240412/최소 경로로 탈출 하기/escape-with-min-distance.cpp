e7551926fe8ef57868a78879fcda1ce57c1c447f
woongseong9393 <130616834+woongseong9393@users.noreply.github.com>
2024-04-12 20:26:44 +0900

    Title: 최소 경로로 탈출 하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240412/최소 경로로 탈출 하기/escape-with-min-distance.cpp
+++ 240412/최소 경로로 탈출 하기/escape-with-min-distance.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#include <algorithm>
+#include <cstring>
 
 using namespace std;
 
 int N, M;
 int mp[100][100] = {0,};
-int step[100][100] = {0,};
-bool visited[100][100] = {0,};
+int visited[100][100];
 
 int dx[4] = {0, 1, 0, -1};
 int dy[4] = {1, 0, -1, 0};
@@ -19,23 +18,14 @@ bool inRange(int x, int y){
     return 0 <= x && x < M && 0 <= y && y < N;
 }
 
+void BFS(int x, int y, int step){
+    for(int i = 0; i < 4; i++){
+        int nx = x + dx[i];
+        int ny = y + dy[i];
 
-
-void BFS(){
-    while(!q.empty()){
-        int curr_x = q.front().first;
-        int curr_y = q.front().second;
-        q.pop();
-
-        for(int i = 0; i < 4; i++){
-            int nx = curr_x + dx[i];
-            int ny = curr_y + dy[i];
-
-            if(inRange(nx, ny) && visited[ny][nx] == 0 && mp[ny][nx] == 1){
-                visited[ny][nx] = true;
-                step[ny][nx] = step[curr_y][curr_x] + 1;
-                q.push(make_pair(nx, ny));
-            }
+        if(inRange(nx, ny) && mp[ny][nx] == 1 && visited[ny][nx] < 0){
+            visited[ny][nx] = step+1;
+            BFS(nx, ny, step + 1);
         }
     }
 
@@ -51,10 +41,10 @@ int main() {
         }
     }
 
-    visited[0][0] = 1;
-    step[N-1][M-1] = -1;
-    q.push({0, 0});
-    BFS();
+    memset(visited, -1, sizeof(visited));
+
+    visited[0][0] = 0;
+    BFS(0, 0, 0);
 
     /*
     for(int i = 0; i < N; i++){
@@ -65,7 +55,7 @@ int main() {
     }
     */
     
-    cout << step[N-1][M-1]; 
+    cout << visited[N-1][M-1];
 
     return 0;
 }
