94c993aba2d551c7bc10048bdce9077456b3b208
woongseong9393 <130616834+woongseong9393@users.noreply.github.com>
2024-04-13 22:55:27 +0900

    Title: 나이트, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240413/나이트/knight-movements.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+#include <cstring>
+
+using namespace std;
+
+int N;
+int dx[8] = {1, 2, 2, 1, -1, -2, -2, -1};
+int dy[8] = {-2, -1, 1, 2, 2, 1, -1, -2};
+bool visited[100][100] = {0,};
+int step[100][100] = {0, };
+
+bool inRange(int x, int y){
+    return 0 <= x && x < N && 0 <= y && y < N;
+}
+
+queue<pair<int, int>> q;
+
+void BFS(){
+    while(!q.empty()){
+        int curr_x = q.front().first;
+        int curr_y = q.front().second;
+        q.pop();
+
+        for(int i = 0; i < 8; i++){
+            int nx = curr_x + dx[i];
+            int ny = curr_y + dy[i];
+
+            if(inRange(nx, ny) && step[ny][nx] < 0){
+                //visited[ny][nx] = 1;
+                step[ny][nx] = step[curr_y][curr_x] + 1;
+                
+                q.push(make_pair(nx, ny));
+            }
+        }
+    }
+}
+
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> N;
+    int r1, c1, r2, c2;
+    cin >> r1 >> c1 >> r2 >> c2;
+
+    //visited[r1-1][c1-1] = 1;
+    memset(step, -1, sizeof(step));
+    q.push(make_pair(c1-1, r1-1));
+
+    step[r2-1][c2-1] = -1;
+    step[r1-1][c1-1] = 0;
+
+    BFS();
+
+    cout << step[r2-1][c2-1];
+    /*
+    for(int i = 0 ; i < N; i++){
+        for(int j = 0; j < N; j++){
+            cout << step[i][j] << ' ';
+        }
+        cout << endl;
+    }
+    */
+
+
+    return 0;
+}
