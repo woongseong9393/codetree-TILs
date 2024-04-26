e4f517e6fe9908f5dd8e283ad4b7ed68dbcc7b4b
woongseong9393 <130616834+woongseong9393@users.noreply.github.com>
2024-04-26 09:09:47 +0900

    Title: 원 모양으로 되어있는 방, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240426/원 모양으로 되어있는 방/a-room-in-a-circle.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N;
+int arr[1003];
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> N;
+    int res = 100301;
+
+    for(int i = 0; i < N; i++){
+        cin >> arr[i];
+    }
+
+    for(int i = 0; i < N; i++){
+        int sum = 0;
+        for(int j = 0; j < N; j++){
+            if(j < i){
+                sum += arr[j] * (N - (i - j));
+            } else{
+                sum += arr[j] * (j - i);
+            }
+        }
+        res = min(res, sum);
+    }
+
+    cout << res;
+    return 0;
+}
