a6046343e94ce48b6359d0e10ed10b92c171605f
woongseong9393 <130616834+woongseong9393@users.noreply.github.com>
2024-04-26 09:11:01 +0900

    Title: 원 모양으로 되어있는 방, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240426/원 모양으로 되어있는 방/a-room-in-a-circle.cpp
+++ 240426/원 모양으로 되어있는 방/a-room-in-a-circle.cpp
@@ -7,7 +7,7 @@ int arr[1003];
 int main() {
     // 여기에 코드를 작성해주세요.
     cin >> N;
-    int res = 100301;
+    int res = 1003 * 1003 * 100;
 
     for(int i = 0; i < N; i++){
         cin >> arr[i];
